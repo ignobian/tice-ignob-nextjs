ac14f3d83b50be730697331b74292b26b5c4432e
Matthijs Kralt <matthijs29@live.com>
2020-03-24 15:08:20 +0100

    Reconfigure config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import getConfig from 'next/config';
 
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://seoblog.com' : publicRuntimeConfig.API_DEVELOPMENT;
+export const API = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.API_PRODUCTION : publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   publicRuntimeConfig: {
-    APP_NAME: 'GRAVINASH',
+    APP_NAME: 'Ignob.com',
     API_DEVELOPMENT: 'http://localhost:8000/api',
+    API_PRODUCTION: 'http://ignob-backend.herokuapp.com/api',
     PRODUCTION: process.env.NODE_ENV === 'production',
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-    DOMAIN_PRODUCTION: 'https://seoblog.com',
+    DOMAIN_PRODUCTION: 'https://ignob-blog-site.herokuapp.com',
     FB_APP_ID: '',
     DISQUS_SHORTNAME: 'gravinash-blog'
   }
