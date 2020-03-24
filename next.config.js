dd96c9dd1dc460de4d131b826e4020d46ab560a4
Matthijs Kralt <matthijs29@live.com>
2020-03-24 15:18:55 +0100

    Change api address to load over https

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: 'Ignob.com',
     API_DEVELOPMENT: 'http://localhost:8000/api',
-    API_PRODUCTION: 'http://ignob-backend.herokuapp.com/api',
+    API_PRODUCTION: 'https://ignob-backend.herokuapp.com/api',
     PRODUCTION: process.env.NODE_ENV === 'production',
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
     DOMAIN_PRODUCTION: 'https://ignob-blog-site.herokuapp.com',
