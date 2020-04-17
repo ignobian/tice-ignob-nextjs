7712122313373193cee851881beb4f0b466198c6
TravelingTice <matthijs29@live.com>
2020-04-17 13:39:46 +0200

    Add error message display in activate page

--- pages/user/activate/[token].js
+++ pages/user/activate/[token].js
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 import Layout from '../../../components/Layout';
 import Router, { withRouter } from 'next/router';
 import { signup } from '../../../actions/auth';
-import Loading from '../../../components/Loading';
 import Head from 'next/head';
 import { APP_NAME } from '../../../config';
+import Link from 'next/link';
+import { DefaultLink } from '../../../components/Link';
+import { Container, Row, Col } from 'reactstrap';
 
 const ActivateAccount = ({ router }) => {
 
@@ -16,11 +18,10 @@ const ActivateAccount = ({ router }) => {
   )
 
   const [values, setValues] = useState({
-    error: '',
-    loading: true
+    error: ''
   });
 
-  const { error, loading } = values;
+  const { error } = values;
 
   useEffect(() => {
     if (router.query.token) {
@@ -35,18 +36,24 @@ const ActivateAccount = ({ router }) => {
     }
   }, [router]);
 
-  const showLoading = () => loading && <Loading/>
-  const showError = () => error && <div className="alert alert-danger">{error}</div>
-
   return (
     <>
       {head()}
       <Layout>
-        {!error && (
-          <h2 className="mt-5">Getting things ready...</h2>
-        )}
-        {showError()}
-        {showLoading()}
+        <Container>
+          <Row>
+            <Col xs="12">
+              {!error ? (
+                <h5 className="mt-5">Getting things ready...</h5>
+              ) : (
+                <>
+                  <p className="text-muted mt-5 mb-3">Something went wrong...</p>
+                  <p className="text-muted">Please <Link href="/signup"><DefaultLink>sign up</DefaultLink></Link> again</p>
+                </>
+              )}
+            </Col>
+          </Row>
+        </Container>
       </Layout>
     </>
   )
