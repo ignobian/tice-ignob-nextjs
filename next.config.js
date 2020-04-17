434125c55a023ce672308666bd591347fa6c4832
TravelingTice <matthijs29@live.com>
2020-04-17 12:35:46 +0200

    Add config new url for the new api

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: 'Ignob.com',
-    API_DEVELOPMENT: 'http://localhost:8000/api',
-    API_PRODUCTION: 'https://ignob-backend.herokuapp.com/api',
+    API_DEVELOPMENT: 'http://localhost:3001/v1',
+    API_PRODUCTION: 'https://ignob-backend-rails-api.herokuapp.com/v1',
     PRODUCTION: process.env.NODE_ENV === 'production',
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
     DOMAIN_PRODUCTION: 'https://ignob-blog-site.herokuapp.com',
