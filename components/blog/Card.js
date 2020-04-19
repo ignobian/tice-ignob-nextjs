7ef191fe56294d45f3312ea129323dfe16366629
TravelingTice <matthijs29@live.com>
2020-04-19 11:35:05 +0200

    Fixed a lot of bugs

--- components/blog/Card.js
+++ components/blog/Card.js
@@ -5,6 +5,7 @@ import { ButtonOutlineLink, CategoryBtn, TagBtn, NoButton } from '../Button';
 import styled from 'styled-components';
 import { DefaultLink } from '../Link';
 import FollowButton from '../FollowButton';
+import { Image, Transformation } from 'cloudinary-react';
 
 const GridContainer = styled.div`
   display: grid;
@@ -17,7 +18,7 @@ const GridContainer = styled.div`
   }
 `;
 
-const FeaturedImg = styled.img`
+const FeaturedImg = styled(Image)`
   height: 150px;
   width: 100%;
   object-fit: cover;
@@ -40,7 +41,7 @@ const SmallCategoryBtn = styled(CategoryBtn)`
 
 const Card = ({ blog }) => {
   const followLink = () => (
-    <FollowButton noborder user={blog.postedBy} />
+    <FollowButton noborder user={blog.user} />
   );
 
   const showCategories = blog => (
@@ -56,8 +57,8 @@ const Card = ({ blog }) => {
   const showTags = blog => (
     blog.tags.map((tag, i) => (
       <div key={i} style={{marginTop: 23}}>
-        <Link href={`/tags/${tag}`}>
-          <TagBtn className="mr-2">{tag}</TagBtn>
+        <Link href={`/tags/${tag.slug}`}>
+          <TagBtn className="mr-2">{tag.name}</TagBtn>
         </Link>
       </div>
     ))
@@ -66,14 +67,20 @@ const Card = ({ blog }) => {
   return (
     <GridContainer>
       <div>
-        <Link href={`/${blog.slug}`}><DefaultLink><FeaturedImg src={`${API}/blog/photo/${blog.slug}`} alt={blog.title}/></DefaultLink></Link>
+        <Link href={`/${blog.slug}`}>
+          <DefaultLink>
+            <FeaturedImg publicId={blog.photo && blog.photo.key} alt={blog.title}>
+              <Transformation width="1000" crop="fill" />
+            </FeaturedImg>
+          </DefaultLink>
+        </Link>
       </div>
 
       <div className="details">
         <Link href={`/${blog.slug}`}><h5 style={{ cursor: 'pointer' }}>{blog.title}</h5></Link>
         <p className="m-0" style={{overflow: 'hidden'}}>{blog.mdesc}...</p>
         <p className="text-muted pt-2 mb-1">
-          <small>Posted by <Link href={`/profile/${blog.postedBy.uniqueUsername}`}><DefaultLink>{blog.postedBy.username}</DefaultLink></Link> | {followLink()} | {moment(blog.updatedAt).fromNow()}</small>
+          <small>Posted by <Link href={`/profile/${blog.user.username}`}><DefaultLink>{blog.user.username}</DefaultLink></Link> | {followLink()} | {moment(blog.updatedAt).fromNow()}</small>
           <span className="d-block d-sm-inline d-md-block d-lg-inline my-2 mt-lg-0">
             <span className="ml-2 ml-sm-3 mr-2">{blog.claps.length}</span>
             <img width="27" src="/images/clap.svg" alt="claps"/>
