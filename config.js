c772478bfa03f6848d362c06f7d670f2bac7741f
TravelingTice <matthijs29@live.com>
2020-05-05 18:14:35 +0200

    Add dev and prod different facebook ids

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
 export const DOMAIN = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.DOMAIN_PRODUCTION : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+export const FB_APP_ID = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.FB_APP_ID_PROD : publicRuntimeConfig.FB_APP_ID_DEV;
 
 export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
 
