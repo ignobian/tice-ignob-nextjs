3742ee97ca2b15a1c56e271be4bf893bd2f34878
TravelingTice <matthijs29@live.com>
2020-07-05 16:04:42 +0200

    Add good styles for conversation listing

--- components/chat/ConversationsList.js
+++ components/chat/ConversationsList.js
@@ -6,24 +6,32 @@ import { Container, Row, Col } from 'reactstrap';
 
 const ConversationsList = () => {
   const [convos, setConvos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const token = getCookie('token');
 
   useEffect(() => {
     getConversations(token).then(data => {
       setConvos(data);
+      setLoading(false);
     });
   }, []);
+
   return (
     <div>
       <Container className="border-bottom">
         <Row>
           <Col xs="12">
             <h3 className="py-3">My conversations</h3>
-
           </Col>
         </Row>
       </Container>
+
+      {loading && (
+        <div className="d-flex justify-content-center">
+          <img src="/images/loading.svg" width="30" className="mt-3" alt="Loading..."/>
+        </div>
+      )}
 
       {convos.map(convo => (
         <Convo convo={convo} />
--- components/chat/Convo.js
+++ components/chat/Convo.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Image, Transformation } from 'cloudinary-react';
+import styled from 'styled-components';
+import Router from 'next/router';
+
+const StyledDiv = styled.div`
+  &:hover {
+    background-color: #efefef;
+    transition: background-color, 200ms ease-out;
+  }
+`;
 
 const Convo = ({ convo }) => {
-
   const showProfileImg = () => (
     <Image onClick={() => Router.push(`/profile/${convo.with.id}`)} width="30" height="30" style={{borderRadius: '50%', objectFit: 'cover', cursor: 'pointer'}} publicId={convo.with.photo}>
       <Transformation width="300" crop="fill" />
@@ -10,13 +18,13 @@ const Convo = ({ convo }) => {
   )
 
   return (
-    <div className="border-bottom py-2 d-flex align-items-center">
+    <StyledDiv role="button" onClick={() => Router.push(`/conversations/${convo.id}`)} className="border-bottom py-2 d-flex align-items-center">
       <div className="p-2">
         {showProfileImg()}
       </div>
 
       <p className="m-0">{convo.with.username}</p>
-    </div>
+    </StyledDiv>
   )
 }
 
