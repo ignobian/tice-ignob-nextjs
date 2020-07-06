6efd29f9996d00674d7984ecb5a23f13569a4633
TravelingTice <matthijs29@live.com>
2020-07-06 20:47:04 +0200

    Add back link

--- components/chat/ChatTop.js
+++ components/chat/ChatTop.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useContext } from 'react'
 import { ConversationContext } from '../../contexts/ConversationContext'
+import { DefaultLink } from '../Link';
+import Link from 'next/link';
 
 const ChatTop = () => {
   const { convoUser } = useContext(ConversationContext);
   return (
     <div>
       {convoUser ? (
-        <div className="p-3 border-bottom">
+        <div className="p-3 border-bottom d-flex justify-content-between">
           <p className="m-0"><span className="text-muted font-italic">Conversation with </span><span className="font-weight-bold">{convoUser.username}</span></p>
-          {convoUser.lastSeen && <p className="text-muted mt-2"><small>Last seen {convoUser.lastSeen}</small></p>}
+          <Link href='/conversations/list'><DefaultLink>Back</DefaultLink></Link>
         </div>
       ) : <div className="d-flex align-items-center justify-content-center"><img src="/images/loading.svg" width="40" /></div>}
     </div>
